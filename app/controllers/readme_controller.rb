ffcea8f960399ef9886a5c3bc88c91286988b16a
Aleksandr Dembskii <dembskoi@gmail.com>
2016-05-21 23:51:56 +0300

    Styles for datatables

--- app/controllers/readme_controller.rb
+++ app/controllers/readme_controller.rb
@@ -3,8 +3,15 @@ class ReadmeController < ApplicationController
     filename = File.join(Rails.root, 'README.md')
     begin
       file = File.open(filename)
-      @markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, space_after_headers: true)
-      render text: @markdown.render(file.read.force_encoding('UTF-8')), layout: true
+      @markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
+                                          autolink: true,
+                                          space_after_headers: true,
+                                          fenced_code_blocks: true,
+                                          underline: true,
+                                          highlight: true,
+                                          footnotes: true,
+                                          tables: true)
+      render text: @markdown.render(file.read.force_encoding('UTF-8')).html_safe, layout: true
     rescue Errno::ENOENT
       render file: 'public/404.html', status: 404
     end
